50ff558299c774e982407dadd04891c38be3504e
root <emendi>
2024-05-23 12:39:40 +0000

    generated output_nsys.nsys-rep to see the backend libraries it uses

--- elementwise_profile.py
+++ elementwise_profile.py
@@ -3,15 +3,15 @@ import torch
 # Initialize two random 3x3 matrices on the GPU
 A = torch.rand((3, 3), device='cuda')
 B = torch.rand((3, 3), device='cuda')
-
+torch.cuda.profiler.cudart().cudaProfilerStart()
 # Start the NVTX range for profiling
-torch.cuda.nvtx.range_push("start")
 
 # Perform element-wise addition
 C = torch.add(A, B)
 
 # End the NVTX range for profiling
-torch.cuda.nvtx.range_pop()
+
+torch.cuda.profiler.cudart().cudaProfilerStop()
 
 # Print the matrices
 print("Matrix A:\n", A)
